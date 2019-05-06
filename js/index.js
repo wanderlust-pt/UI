da4431ca6d42326ec729b3a501144bc5518c96a1
Myke47 <mychel.larson@gmail.com>
2019-05-06 16:46:13 -0700

    commit event listeners

---
+++ js/index.js
@@ -0,0 +1,75 @@
+
+let teamMem = document.querySelector(".team-mem");
+
+teamMem.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'wheat';
+    event.target.style.backgroundColor = '#db4c3f';
+    event.target.style.borderColor = '#db4c3f';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+        event.target.style.backgroundColor = "";
+        event.target.style.borderColor = "";
+
+    }, 5000);
+})
+
+let teamMem2 = document.querySelector(".team-mem:nth-child(2)");
+
+teamMem2.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'wheat';
+    event.target.style.backgroundColor = '#db4c3f';
+    event.target.style.borderColor = '#db4c3f';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+        event.target.style.backgroundColor = "";
+        event.target.style.borderColor = "";
+
+    }, 5000);
+})
+
+let teamMem3 = document.querySelector(".team-mem:nth-child(3)");
+
+teamMem3.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'wheat';
+    event.target.style.backgroundColor = '#db4c3f';
+    event.target.style.borderColor = '#db4c3f';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+        event.target.style.backgroundColor = "";
+        event.target.style.borderColor = "";
+
+    }, 5000);
+})
+
+let teamMem4 = document.querySelector(".team-mem:nth-child(4)");
+
+teamMem4.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'wheat';
+    event.target.style.backgroundColor = '#db4c3f';
+    event.target.style.borderColor = '#db4c3f';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+        event.target.style.backgroundColor = "";
+        event.target.style.borderColor = "";
+
+    }, 5000);
+})
+
+let teamMem5 = document.querySelector(".team-mem:nth-child(5)");
+
+teamMem5.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'wheat';
+    event.target.style.backgroundColor = '#db4c3f';
+    event.target.style.borderColor = '#db4c3f';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+        event.target.style.backgroundColor = "";
+        event.target.style.borderColor = "";
+
+    }, 5000);
+})
